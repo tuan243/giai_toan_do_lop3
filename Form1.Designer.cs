454ed24da1786cbe5c9a652b0c2882ebadce9a88
minh tuan do <tuanquavip243@gmail.com>
2018-11-24 20:59:13 +0700

    sua phep chia va them try catch

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(118, 57);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(374, 82);
@@ -44,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(508, 55);
+            this.button1.Location = new System.Drawing.Point(392, 71);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -55,17 +56,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(115, 162);
+            this.label1.Location = new System.Drawing.Point(12, 101);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Bài giải:\r\n";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 128);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(371, 86);
+            this.textBox2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(523, 240);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -81,6 +92,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,49 +22,57 @@ namespace toandolop3csharp
         }
 
         void OneExpressionMath()
-        {
-            label1.Text = "Bài Giải:\n";
+        {            
             string donvi = "";
             string doituong2;
             string operand1;
             string operand2;
 
             string[] sentences = problem.Split(',', '.','?');
-
-            doituong2 = sentences[2].Substring(sentences[2].IndexOf("Hỏi") + 4, sentences[2].IndexOf("bao nhiêu") - sentences[2].IndexOf("Hỏi") - 5);
-            donvi = sentences[2].Substring(sentences[2].IndexOf("bao nhiêu") + 10);
+            try
+            {
+                doituong2 = sentences[2].Substring(sentences[2].IndexOf("Hỏi") + 4, sentences[2].IndexOf("bao nhiêu") - sentences[2].IndexOf("Hỏi") - 5);
+                donvi = sentences[2].Substring(sentences[2].IndexOf("bao nhiêu") + 10);
+            }
+            catch
+            {
+                textBox2.AppendText("Không giải được :))");
+                return;
+            }
             operand1 = Regex.Match(sentences[0], @"\d+").Value;
             string temp = Regex.Match(sentences[1], @"[1-9]/[1-9]").Value;
+            bool isMultiplication = true;
             if (temp != "")
             {
                 operand2 = temp[2].ToString();
+                isMultiplication = false;
             }
             else operand2 = Regex.Match(sentences[1], @"\d+").Value;
 
-            label1.Text += String.Format("Số {0} của {1} là:\n", donvi, doituong2);
+            textBox2.AppendText(String.Format("Số {0} của {1} là:\n", donvi, doituong2));
 
             int result = 0;
-            if (Regex.Match(sentences[1], @"gấp|bằng").Value != "")
+            if (Regex.Match(sentences[1], @" gấp | bằng ").Value != "" && isMultiplication)
             {
                 result = Convert.ToInt32(operand1) * Convert.ToInt32(operand2);
-                label1.Text += String.Format("      {0} x {1} = {2} ({3})\n", operand1, operand2, result, donvi);
+                textBox2.AppendText(String.Format("      {0} x {1} = {2} ({3})\n", operand1, operand2, result, donvi));
             }
-            else if(Regex.Match(sentences[1],@"nhẹ|ít|ngắn|thấp").Value != "")
+            else if(Regex.Match(sentences[1],@" nhẹ | ít | ngắn | thấp ").Value != "")
             {
                 result = Convert.ToInt32(operand1) - Convert.ToInt32(operand2);
-                label1.Text += String.Format("      {0} - {1} = {2} ({3})\n", operand1, operand2, result, donvi);
+                textBox2.AppendText(String.Format("      {0} - {1} = {2} ({3})\n", operand1, operand2, result, donvi));
             }
-            else if(Regex.Match(sentences[1],@"nặng|nhiều|dài|cao").Value != "")
+            else if(Regex.Match(sentences[1],@" nặng | nhiều | dài | cao ").Value != "")
             {
                 result = Convert.ToInt32(operand1) + Convert.ToInt32(operand2);
-                label1.Text += String.Format("      {0} + {1} = {2} ({3})\n", operand1, operand2, result, donvi);
+                textBox2.AppendText(String.Format("      {0} + {1} = {2} ({3})\n", operand1, operand2, result, donvi));
             }
             else
             {
                 result = Convert.ToInt32(operand1) / Convert.ToInt32(operand2);
-                label1.Text += String.Format("      {0} : {1} = {2} ({3})\n", operand1, operand2, result, donvi);
+                textBox2.AppendText(String.Format("      {0} : {1} = {2} ({3})\n", operand1, operand2, result, donvi));
             }
-            label1.Text += String.Format("Đáp số: {0} {1}", result, donvi);
+            textBox2.AppendText(String.Format("Đáp số: {0} {1}", result, donvi));
         }
 
         private void button1_Click(object sender, EventArgs e)
